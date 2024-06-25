d5c4b89680450797dfa280237f3d9c649fbe7547
Sayinko <lauradelyannis@gmail.com>
2024-06-25 11:36:57 +0200

    ajout fonction

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,74 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
-const flecheDroite = document.querySelector(".arrow_right")
-const flecheGauche = document.querySelector(".arrow_left")
-const bannerImg = document.querySelector(".banner-img")
-const bannerText = document.querySelector("#banner p")
+index = 0;
 
-let i = 0
+const flecheDroite = document.createElement("img");
+const flecheGauche = document.createElement("img");
+const banner = document.querySelector("#banner");
+const dots = document.querySelector(".dots");
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+
+flecheDroite.src = `./assets/images/arrow_right.png`;
+flecheGauche.src = `./assets/images/arrow_left.png`;
+
+flecheDroite.classList.add("arrow", "arrow_right");
+flecheGauche.classList.add("arrow", "arrow_left");
+
+banner.appendChild(flecheDroite);
+banner.appendChild((l = flecheGauche));
 
 flecheDroite.addEventListener("click", () => {
-	i++
-	if ( i >= slides.length) {
-		i = 0
-	}
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
 
-	//bannerImg.src = `./assets/images/slideshow/${slides[i].image}` ne marche pas !
-	bannerText.innerHTML = slides[i].tagLine
-	console.log("j'ai cliqué tant de fois : " + i)
-})
-
-// if (i = 0, i >= slides.length, i++) { i = 0}
-// bannerText.innerHTML = slides[i].tagLine  pourquoi celui plus haut (dans flechedroite) marche et pas celui la ? 
-
+  updateSlides();
+});
 
 flecheGauche.addEventListener("click", () => {
-	i--
-	if (i < 0) {
-		i = slides.length - 1
-	}
-	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`
-	bannerText.innerHTML = slides[i].tagLine
-	console.log("Vous avez cliquez tant de fois : " +i)
- })
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
 
+  updateSlides();
+});
 
- const bulletPointsParent = document.querySelector(".dots")
+for (i = 0; i < slides.length; i++) {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dots.appendChild(dot);
 
-
-
-for (i = 0; i > slides.length; i++) {
-	const iSlides = slides[i]
-	const bulletPoints = document.createElement("div")
-	bulletPointsParent.appendChild(bulletPoints)
-	bulletPoints.classList.add("dot")
+  if (i === 0) {
+    dot.classList.add("dot_selected");
+  }
 }
 
-//for (i = 0; i > slides.length; i ++) {
+function updateSlides() {
+  bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+  bannerText.innerHTML = slides[index].tagLine;
 
-	//if (i==0) {
-		//dots.insertAdjacentElementHTML(`beforeend`, `<div class="dot dot_selected"></div>`)
-	//}
-	//else {
-		//dots.insertAdjacentElementHTML(`beforeend`, `<div class="dot"></div>`)
-	//}
-	//bulletPoints.innerHTML = slides[i]
-//}
+  document.querySelector(".dot_selected").classList.remove("dot_selected");
+  const dot = document.querySelectorAll(".dot");
+  dot[index].classList.add("dot_selected");
+}
